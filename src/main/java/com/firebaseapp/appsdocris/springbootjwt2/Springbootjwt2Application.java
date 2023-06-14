7c725781a0be46cb509f18f5cca4f4e876aa9924
Cristhiano Roberto <bangomc@gmail.com>
2023-06-13 23:55:55 -0300

    First commit

---
+++ src/main/java/com/firebaseapp/appsdocris/springbootjwt2/Springbootjwt2Application.java
@@ -0,0 +1,13 @@
+package com.firebaseapp.appsdocris.springbootjwt2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootjwt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootjwt2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/firebaseapp/appsdocris/springbootjwt2/Springbootjwt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firebaseapp.appsdocris.springbootjwt2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootjwt2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
